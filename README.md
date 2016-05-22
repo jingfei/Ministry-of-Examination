494b196340f28b1ac1e66a0622eb21a8a4d4bf71
jingfei <jingfei955047@gmail.com>
2016-05-22 15:46:40 -0700

    Finish first section.

---
+++ README.md
@@ -0,0 +1,25 @@
+# igemncku
+
+## Develop
+
+```
+$ sudo npm install
+```
+
+build js, css
+
+```
+$ gulp
+```
+
+build template
+
+```
+$ ./bin/build
+```
+
+deploy 
+
+```
+git subtree push --prefix public origin gh-pages
+````
---
+++ route.js
@@ -0,0 +1,18 @@
+var path = require('path');
+var url = require('url');
+
+route = [
+  {
+    data: {
+      "title": "Ministry of Examination R.O.C",
+      "url": "",
+      "img": "",
+      "description": "",
+    },
+    "partials": './partials.js',
+    "layout":  "./view/index.hbs",
+    "filename": "./public/index.html"
+  }
+];
+
+module.exports = route;
