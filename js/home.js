48d0959c9011cc503f7e5672072adf2593b23f66
jingfei <jingfei955047@gmail.com>
2016-05-23 00:26:56 -0700

    Add icon to home page.

--- js/home.js
+++ js/home.js
@@ -1,9 +1,13 @@
+var lastTop = 0;
+var section = 1;
+var canScroll = true;
+
 (function() {
 	document.getElementById("dialog-title").addEventListener("click", function() {
 		document.getElementById("dialog-title").style.display = "none";
 		document.getElementById("dialog-content").style.display = "block";
 	});
-	document.getElementsByClassName("glyphicon-remove")[0].addEventListener("click", function() {
+	document.getElementById("dialog-content-title").addEventListener("click", function() {
 		document.getElementById("dialog-title").style.display = "block";
 		document.getElementById("dialog-content").style.display = "none";
 	});
